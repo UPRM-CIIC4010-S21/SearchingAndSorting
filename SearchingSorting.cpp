397f292646a65f5d4ae03287a5532cef2c19c2d0
Bienvenido.Velez <Bienvenido.Velez@upr.edu>
2021-04-21 17:49:01 -0400

    added Selection sort and Insertion sort

--- SearchingSorting.cpp
+++ SearchingSorting.cpp
@@ -41,13 +41,48 @@ int binarySearch(int key, int numbers[], int size)
 }
 
 
+void selectionSort(int *numbers, int size)
+{
+
+    for (int i=0; i<size; i++) {
+
+        int minPos = i;
+        for(int j=i; j<size; j++) {
+            if (numbers[j] < numbers[minPos]) {
+                minPos = j;
+            }
+        }
+        int temp = numbers[i];
+        numbers[i] = numbers[minPos];
+        numbers[minPos] = temp;
+    }
+
+}
+
+void insertionSort(int *numbers, int size) 
+{
+    for (int i=1; i<size; i++) {
+        int j = i;
+        while ((j >= 0)  && (numbers[j] < numbers[j-1])) {
+            int temp = numbers[j];
+            numbers[j] = numbers[j-1];
+            numbers[j-1] = temp;
+            j--;
+        }
+    }
+}
+
+
 int main() 
 {
 
     int myNumbers[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int unsorted[10] { 0, 9, 2, 7, 4, 5, 6, 1, 8, 3 };
 
     cout << "Position of 0 is: " << binarySearch(0, myNumbers, 10) << endl;
     cout << "Position of 4 is: " << binarySearch(4, myNumbers, 10) << endl;
     cout << "Position of 10 is: " << binarySearch(10, myNumbers, 10) << endl;
+
+    insertionSort(unsorted, 10);
 
 }
